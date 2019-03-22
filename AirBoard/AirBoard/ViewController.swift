8f54ff738590aff32e19f51c35affc653cc10b57
Kirill <44346241+FutureAndroidDeveloper@users.noreply.github.com>
2019-03-22 16:35:51 +0300

    Create new empty project

---
+++ AirBoard/AirBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirBoard
+//
+//  Created by Кирилл Клименков on 3/22/19.
+//  Copyright © 2019 Кирилл Клименков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
