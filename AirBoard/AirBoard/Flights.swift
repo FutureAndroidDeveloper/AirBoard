be938b61a750a476b585042db6e7cb84bcbcf48e
Kirill <44346241+FutureAndroidDeveloper@users.noreply.github.com>
2019-03-25 14:32:01 +0300

    Created Data Model for Flights

---
+++ AirBoard/AirBoard/Flights.swift
@@ -0,0 +1,25 @@
+//
+//  Flights.swift
+//  AirBoard
+//
+//  Created by Кирилл Клименков on 3/25/19.
+//  Copyright © 2019 Кирилл Клименков. All rights reserved.
+//
+
+import Foundation
+class Flight {
+    
+    // MARK: Properties
+    
+    var departure: String?
+    var arrival: String?
+    var departureTime: Int?
+    var arrivalTime: Int?
+    
+    init(departure: String?, arrival: String?, departureTime: Int?, arrivalTime: Int?) {
+        self.departure = departure
+        self.arrival = arrival
+        self.departureTime = departureTime
+        self.arrivalTime = arrivalTime
+    }
+}
